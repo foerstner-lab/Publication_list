95c37be1802aa4d7410a91e1f2b3376c8fa4d471
Konrad Förstner <konrad@foerstner.org>
2019-12-16 20:27:47 +0100

    Add shell script to retrieve short DOIs

---
+++ generate_shortDOI.sh
@@ -0,0 +1,7 @@
+for DOI in $(cut -f 1 Publications.tsv | grep -v DOI)
+do
+    SHORT_DOI=$(curl -s "http://shortdoi.org/${DOI}"  | grep "http://doi.org/" | sed -e "s/.*doi.org\///" -e "s/<.*//" 2> /dev/null)
+    printf "${DOI}\t${SHORT_DOI}\n"
+done
+
+# 
