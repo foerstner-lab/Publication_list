a6977617b90e6280692f031634c467101772fc3f
Konrad Förstner <konrad@foerstner.org>
2020-01-09 06:52:27 +0100

    Clean up script

--- generate_shortDOI.sh
+++ generate_shortDOI.sh
@@ -3,5 +3,3 @@ do
     SHORT_DOI=$(curl -s "http://shortdoi.org/${DOI}"  | grep "http://doi.org/" | sed -e "s/.*doi.org\///" -e "s/<.*//" 2> /dev/null)
     printf "${DOI}\t${SHORT_DOI}\n"
 done
-
-# 
